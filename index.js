7209cc2c2ba4cfc078dcf2e89c1bfe6840379db7
Lucy <97576370+lgaceta@users.noreply.github.com>
2024-03-11 06:50:58 +0000

    Long & Lat hw extension

--- index.js
+++ index.js
@@ -3,50 +3,57 @@ const https = require("https");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(bodyParser.urlencoded({extended: true}));
+// week9 openweather HW extension: create html form to take Longitude and Latitude and pass those pieces of data tp the index file. From here make a api request that displays weather conditions based on html input// 
 
-//displays index.html of root path
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html")
+app.get("/", function (_req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-//invoked after hitting go in the html form
-app.post("/", function(req, res) {
-    
-    // takes in the zip from the html form, display in // console. Takes in as string, ex. for zip 02139
-        var zip = String(req.body.zipInput);
-        console.log(req.body.zipInput);
-    
-    //build up the URL for the JSON query, API Key is // secret and needs to be obtained by signup 
-        const units = "imperial";
-        const apiKey = "67f6b382921c1e89b39b20d4f9556f22";
-        const url = "https://api.openweathermap.org/data/2.5/weather?zip=" + zip +  "&units=" + units + "&APPID=" + apiKey;
-    
-    // this gets the data from Open WeatherPI
-    https.get(url, function(response){
-        console.log(response.statusCode);
-        
-        // gets individual items from Open Weather API
-        response.on("data", function(data){
-            const weatherData = JSON.parse(data);
-            const temp = weatherData.main.temp;
-            const city = weatherData.name;
-            const weatherDescription = weatherData.weather[0].description;
-            const icon = weatherData.weather[0].icon;
-            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-            
-            // displays the output of the results
-            res.write("<h1> The weather is " + weatherDescription + "<h1>");
-            res.write("<h2>The Temperature in " + city + " " + zip + " is " + temp + " Degrees Fahrenheit<h2>");
-            res.write("<img src=" + imageURL +">");
-            res.send();
-        });
+app.post("/", function (req, res) {
+
+    // create long and lat variables to hold the input from the html form. check the id to make sure parsing is successful//
+    const long = req.body.lonInput;
+    const lat = req.body.latInput;
+
+    const encodedLat = encodeURIComponent(lat);
+    const encodedLong = encodeURIComponent(long);
+
+    const units = "imperial";
+    const mySecret = process.env.API_KEY;
+  
+//used Claude AI to construct the url to take html form input and use it to get the coordinates from the openweather api//
+    const weatherDataURL = `https://api.openweathermap.org/data/2.5/weather? 
+    lat=${encodedLat}&lon=${encodedLong}&APPID=${mySecret}&units=${units}`;
+
+    https.get(weatherDataURL, function (response) {
+      console.log(response.statusCode);
+
+      response.on("data", function (data) {
+        const weatherData = JSON.parse(data);
+        const temp = weatherData.main.temp;
+        const humidity = weatherData.main.humidity;
+        const clouds = weatherData.clouds.all;
+        const weatherDescription = weatherData.weather[0].description;
+        const icon = weatherData.weather[0].icon;
+        const imageURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+
+        res.write("<h1> The weather is " + weatherDescription + "</h1>");
+        res.write(`
+          <h2>
+            The current temperature for this location is ${temp} degrees Fahrenheit. The cloud coverage 
+            is ${clouds}% and the humidity level is ${humidity}%.
+          </h2>
+        `);
+        res.write("<img src=" + imageURL + ">");
+        res.send();
+      });
     });
-})
+ });
 
 
-//Code will run on 3000 or any available open port
-app.listen(process.env.PORT || 3000, function() {
-console.log ("Server is running on port")
+// Code will run on 3000 or any available open port
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server is running on port");
 });
